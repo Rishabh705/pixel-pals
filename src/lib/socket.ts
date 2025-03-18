572f0417a8f501322b6b765dc7d44a201df7f711
Rishabh705 <rishabh0739@gmail.com>
2025-03-18 21:28:19 +0530

    Add reconnect config to socket

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,7 +2,13 @@ import { io } from 'socket.io-client';
 
 const server = import.meta.env.VITE_SERVER;
 
-const socket = io(server);
+const socket = io(server,{
+    reconnection: true,               // Enable auto-reconnect
+    reconnectionAttempts: Infinity,   // Try reconnecting forever
+    reconnectionDelay: 1000,          // Start at 1 second delay
+    reconnectionDelayMax: 5000,       // Max delay of 5 seconds
+    timeout: 20000,                   // 20s before considering connection attempt failed
+});
 
 socket.on('connect', () => {
     console.log('Connected to server with id:', socket.id);
